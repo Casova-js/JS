042cb6cf7955153d1fa8acf0c355e78992c10c2c
Casova-js <johan@casova.eu>
2025-03-30 14:46:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 10000;
 
-// === ÄNDRA DENNA TILL DIN API-NYCKEL ===
-const  = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2NhdGlvbl9pZCI6ImUyc3VKckp0NEE1OXA0UWFoaDFZIiwiY29tcGFueV9pZCI6IkRHbkVsM1B5c3hHYzQxanZ5MWw4IiwidmVyc2lvbiI6MSwiaWF0IjoxNzAzNzczMzM1NjE5LCJzdWIiOiJ1c2VyX2lkIn0.TAJ03agrZf8SWTOFhMzHk1mqWUa-1LfE50fpgFqOxCg'din-verkliga-api-nyckel-här'; // <-- byt ut!
+// === BYT TILL DIN RIKTIGA API-NYCKEL ===
+const GHL_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2NhdGlvbl9pZCI6ImUyc3VKckp0NEE1OXA0UWFoaDFZIiwiY29tcGFueV9pZCI6IkRHbkVsM1B5c3hHYzQxanZ5MWw4IiwidmVyc2lvbiI6MSwiaWF0IjoxNzAzNzczMzM1NjE5LCJzdWIiOiJ1c2VyX2lkIn0.TAJ03agrZf8SWTOFhMzHk1mqWUa-1LfE50fpgFqOxCg"; // <-- GLÖM INTE citattecken!
 
 app.use(cors());
 app.use(bodyParser.json());
